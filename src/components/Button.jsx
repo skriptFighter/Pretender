d8b7f493faf704f3db06b048b59d2c3c8eca111d
SkriptFighter <adelboudjema099@gmail.com>
2024-03-01 15:12:22 +0100

    add: onClick btn

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,7 @@
-function Button({ type, className, children }) {
+function Button({ type, onClick, className, children }) {
  return (
   <button
+   onClick={onClick}
    type={type}
    className={`bg-black text-white hover:bg-gray-900 font-semibold py-2 px-4 rounded w-full ${className}`}
   >
