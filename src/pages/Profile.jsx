0ad8ca24b7128de8dc55307217a07aff554941b5
SkriptFighter <adelboudjema099@gmail.com>
2024-03-05 23:24:10 +0100

    add: husky prettier fix,edit profile form

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,21 +5,27 @@ import { useEditProfile } from "../hooks/useEditProfile"
 import { useUserInfos } from "../hooks/useUserInfos"
 import { useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import Button from "../components/Button"
 
 function Profile() {
  const { editProfile } = useEditProfile()
  const { user } = useUserInfos()
 
- const { register, handleSubmit } = useForm()
+ const {
+  register,
+  handleSubmit,
+  formState: { errors },
+ } = useForm()
 
- const { user: authUser, isAuthenticated } = useAuthUser()
+ const { user: authUser, isAuthenticated, isLoading } = useAuthUser()
  const navigate = useNavigate()
 
  useEffect(
   function () {
-   if (!isAuthenticated) navigate("/login")
+   if (!isAuthenticated && !isLoading) navigate("/login")
+   //  if (isAuthenticated && isLoading) return <p>loading...</p>
   },
-  [isAuthenticated, navigate]
+  [isAuthenticated, navigate, isLoading]
  )
 
  function onSubmit(data) {
@@ -29,43 +35,76 @@ function Profile() {
    user: { ...data, image },
    id: authUser.id,
   })
+  console.log(data)
  }
 
  return (
-  <div>
-   <img src={user?.[0]?.image} />
-   <form onSubmit={handleSubmit(onSubmit)}>
-    <Input
-     htmlForId={"image"}
-     label={"Profile picture"}
-     type={"file"}
-     register={{
-      ...register("image"),
-     }}
-    />
+  <div className="w-1/3 mx-auto flex justify-center items-center h-screen">
+   <div className="bg-zinc-100 py-12 px-8 rounded-lg outline-black shadow-xl">
+    <form onSubmit={handleSubmit(onSubmit)}>
+     <div className="mb-8">
+      <h1 className="text-xl font-semibold">Profile</h1>
+      <p>Update your profile information.</p>
+     </div>
 
-    <Input
-     htmlForId={"username"}
-     label={"username"}
-     type={"text"}
-     defaultValue={user?.[0]?.username}
-     register={{
-      ...register("username"),
-     }}
-    />
+     <div className="flex flex-col gap-5 ">
+      <div className="flex items-center gap-4">
+       <img src={user?.[0]?.image} alt="" className="w-1/4 rounded-full" />
+       <input type="file" {...register("image")} />
+      </div>
 
-    <Input
-     htmlForId={"email"}
-     label={"Email"}
-     type={"text"}
-     defaultValue={user?.[0]?.email}
-     register={{
-      ...register("email"),
-     }}
-    />
+      <div className="flex flex-col">
+       <label htmlFor="username">Username</label>
+       <input
+        id="username"
+        type="text"
+        placeholder="Username"
+        defaultValue={user?.[0]?.username}
+        className="p-2"
+        {...register("username")}
+       />
+      </div>
 
-    <button type="submit">submit</button>
-   </form>
+      <div className="flex flex-col">
+       <label htmlFor="email">Email</label>
+       <input
+        id="email"
+        type="text"
+        placeholder="Email"
+        defaultValue={user?.[0]?.email}
+        disabled={true}
+        className="p-2"
+       />
+      </div>
+
+      <div className="flex flex-col">
+       <label htmlFor="newPassword">New password</label>
+       <input
+        id="newPassword"
+        type="newPassword"
+        placeholder="New password"
+        className="p-2"
+        {...register("newPassword")}
+       />
+      </div>
+
+      <div className="flex flex-col">
+       <label htmlFor="rePassword">Confirm new password</label>
+       <input
+        id="rePassword"
+        type="text"
+        placeholder="Confirm password"
+        className="p-2"
+        {...register("rePassword")}
+       />
+      </div>
+
+      <Button primary={true} type={"submit"}>
+       Save
+      </Button>
+     </div>
+    </form>
+   </div>
   </div>
  )
 }
