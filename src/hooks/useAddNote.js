8e9a4211f2b7858865952b91db0ea680b3ed2b09
SkriptFighter <adelboudjema099@gmail.com>
2024-03-06 22:29:03 +0100

    feat: add notes

---
+++ src/hooks/useAddNote.js
@@ -0,0 +1,18 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query"
+import { addNote as addNoteApi } from "../data/notes"
+import toast from "react-hot-toast"
+
+export function useAddNote() {
+ const queryClient = useQueryClient()
+ const { mutate: addNote } = useMutation({
+  mutationFn: addNoteApi,
+  onSuccess: () => {
+   queryClient.invalidateQueries({ queryKey: ["notes"] })
+  },
+  onError: (err) => {
+   toast.error(err.message)
+  },
+ })
+
+ return { addNote }
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,7 +6,9 @@ import { MdDeleteOutline } from "react-icons/md"
 import { CiImageOn } from "react-icons/ci"
 import { LuPaintbrush } from "react-icons/lu"
 import { VscPinned } from "react-icons/vsc"
+
 import { useSelector } from "react-redux"
+import AddNote from "../ui/AddNote"
 
 function Dashboard() {
  const { notes, isLoading, error } = useNotes()
@@ -29,10 +31,14 @@ function Dashboard() {
  if (error) return <p>error</p>
 
  return (
-  <div className="w-full pr-6 grid grid-cols-6 gap-10">
-   {filteredNotes.map((note) => (
-    <Note title={note.title} content={note.content} key={note.id} />
-   ))}
+  <div className="flex flex-col gap-8">
+   <AddNote />
+
+   <div className="w-full pr-6 flex flex-wrap gap-x-10 gap-y-2">
+    {filteredNotes.map((note) => (
+     <Note title={note.title} content={note.content} key={note.id} />
+    ))}
+   </div>
   </div>
  )
 }
@@ -44,11 +50,11 @@ function Note({ title, content }) {
   <div
    onMouseEnter={() => setIsHover(true)}
    onMouseLeave={() => setIsHover(false)}
-   className={`px-2 pb-1 rounded shadow-md outline-black self-start `}
+   className={`w-56 px-2 pb-1 rounded-lg shadow-md outline-black dark:shadow-sm dark:shadow-slate-800 self-start `}
   >
    <div className="px-4 pt-8">
     <div className="font-semibold text-lg">{title}</div>
-    <p className="break-words pb-8">{content}</p>
+    <p className="break-words pb-8 ">{content}</p>
    </div>
 
    <Options isHover={isHover} />
@@ -56,10 +62,10 @@ function Note({ title, content }) {
  )
 }
 
-function Options({ isHover }) {
+export function Options({ isHover }) {
  return (
   <div
-   className={`items-center  flex justify-between transition-all duration-500 opacity-0 ${isHover && "opacity-100"}`}
+   className={`flex justify-between items-center transition-all duration-300 opacity-0 ${isHover && "opacity-100"}`}
   >
    <Button header={true}>
     <VscPinned fontSize={20} cursor={"pointer"} />
