8aa0d0ed67b4ca676f119fb12d8356b032ed5f15
SkriptFighter <adelboudjema099@gmail.com>
2024-03-10 16:09:00 +0100

    del: trash react-color,feat: my own fucking functioning react-color

---
+++ src/components/ColorPickerr.jsx
@@ -0,0 +1,40 @@
+import { useUpdateBgColor } from "../hooks/useUpdateBgColor"
+import { useClickOutside } from "../hooks/useClickOutside"
+
+const colors = [
+ { id: 1, color: "#f44336" },
+ { id: 2, color: "#0693e3" },
+ { id: 3, color: "#00d084" },
+ { id: 4, color: "#ffeb3b" },
+ { id: 5, color: "#b279d2" },
+ { id: 6, color: "#ff6900" },
+ { id: 7, color: "#e91e63" },
+]
+
+function ColorPicker({ id, setIsPickOpen }) {
+ const { updateBgColor } = useUpdateBgColor()
+ const ref = useClickOutside(() => setIsPickOpen(null))
+
+ const handleColorChange = (color) => {
+  setIsPickOpen(null)
+  updateBgColor({ color, id })
+ }
+
+ return (
+  <div
+   className="flex items-center justify-between gap-3 px-4 py-2 bg-white shadow-md shadow-black absolute -bottom-10 rounded-xl left-1/2 -translate-x-1/2 "
+   ref={ref}
+  >
+   {colors.map((color) => (
+    <div
+     key={color.id}
+     onClick={() => handleColorChange(color.color)}
+     style={{ backgroundColor: color.color }}
+     className="w-7 h-7 rounded-full cursor-pointer hover:scale-125 transition-all duration-300"
+    />
+   ))}
+  </div>
+ )
+}
+
+export default ColorPicker
