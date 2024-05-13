7b3197a8408b254177741caf56c935e29cae6ee3
SkriptFighter <adelboudjema099@gmail.com>
2024-05-14 01:49:30 +0200

    recommit: END OF MVP

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -12,7 +12,7 @@ function Modal({ children }) {
   <div className="fixed left-0 top-0 z-50 flex h-full w-full items-center justify-center bg-primaryDark bg-opacity-50 ">
    <div
     ref={ref}
-    className="phoneSm:w-full phoneLg:w-3/4 w-1/4 min-w-72 rounded-lg bg-white p-4 shadow-lg sm:w-1/2"
+    className="w-1/4 min-w-72 rounded-lg bg-white p-4 shadow-lg phoneSm:w-full phoneLg:w-3/4 sm:w-1/2"
    >
     {children}
    </div>
